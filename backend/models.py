788349a58faa1361a97bdd4d70916569c4783623
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-11-19 11:43:50 +0530

    set up my tabel relation in dtatabase

--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,9 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer , primary_key=True, index=True)
+    title = Column(String,nullable=False) #nullable false means it is mandatory field
+    completed = Column(Boolean, default=False)
