a7ebfc92fdd16ffaaf78cb1a3c4a8e0978ac4e55
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-11-19 17:44:19 +0530

    return correct url

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:bhavesh729@localhost:5432/Task_Manager"
-
+DATABASE_URL =  "postgresql://postgres:bhavesh729@localhost:5432/Task_Manager"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush= False, bind=engine , autocommit=False
 )
