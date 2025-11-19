9f667e6566acf13543d26f3941c245cfe4623ecf
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-11-19 11:43:33 +0530

    imported library

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://user:bhavesh729@localhost:5432/Task_Manager"
 
