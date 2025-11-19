dc4163174ca5eb4c3abcf0b9a51e1567409b9a07
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-11-19 11:33:27 +0530

    created databse structure

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "postgresql://user:bhavesh729@localhost:5432/Task_Manager"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autoflush= False, bind=engine , autocommit=False
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
